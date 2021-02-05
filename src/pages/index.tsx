1042d84876cb4787b4e8367464b91732f70f1630
Andre Grossi <iamandregrossi@gmail.com>
2021-02-05 18:10:30 -0300

    Add getStaticProps on index to populate artists and releases data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,35 @@
-import { FC } from 'react'
+import { GetStaticProps, NextPage } from 'next'
 
-import { Hero } from '~/components'
+import { fetchArtists, fetchLatestReleases } from '~/graphql'
+import { Hero, Artists, Releases } from '~/sections'
 import { Container } from '~/styles/pages'
 
-const IndexPage: FC = () => {
+interface HomeProps {
+  releases: {
+    featured: Release
+    upcoming: Release[]
+    latest: Release[]
+  }
+  artists: Artist[]
+}
+
+const Home: NextPage<HomeProps> = ({ artists, releases }) => {
   return (
     <Container>
       <Hero />
+      <Releases {...releases} />
+      <Artists artists={artists} />
     </Container>
   )
 }
 
-export default IndexPage
+export const getStaticProps: GetStaticProps = async () => {
+  const releases = await fetchLatestReleases()
+  const { artists } = await fetchArtists()
+
+  return {
+    props: { releases, artists }
+  }
+}
+
+export default Home
