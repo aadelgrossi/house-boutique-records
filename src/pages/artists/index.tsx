7b1ca13c00f0f9cb229c9f7f9f2ceacecaf34681
Andre Grossi <iamandregrossi@gmail.com>
2021-02-06 18:36:18 -0300

    Create base artists index page

---
+++ src/pages/artists/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const Artists: React.FC = () => {
+  return (
+    <Container>
+      <h1>artists</h1>
+    </Container>
+  )
+}
+
+export default Artists
