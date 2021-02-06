d4247d76aeb3824ad431c1e2a8172f3caa0ffac3
Andre Grossi <iamandregrossi@gmail.com>
2021-02-06 01:11:31 -0300

    Create useTranslation hook and translations mapping

---
+++ src/translations/index.ts
@@ -0,0 +1,41 @@
+interface Translation {
+  [key: string]: Record<string, string>
+}
+
+export const messages: Translation = {
+  pt: {
+    header_home: 'Home',
+    header_releases: 'Música',
+    header_artists: 'Artistas',
+    header_news: 'Notícias',
+    header_radio: 'Podcast',
+    home_heroTitle:
+      'SEU NOVO LUGAR PARA O MELHOR DO TRIBAL, CIRCUIT & HOUSE MUSIC',
+    home_releasesButton: 'Novos lançamentos',
+    home_artistsButton: 'Conheça os artistas',
+    home_featuredReleaseHeading: 'Destaque',
+    home_latestReleaseHeading: 'Últimos lançamentos',
+    home_upcomingReleasesHeading: 'Próximos lançamentos',
+    home_artistsReadMore: 'Conheça',
+    seeAll: 'Ver todos',
+    footer_socials: 'Redes sociais',
+    footer_contact: 'Fale com a gente'
+  },
+  en: {
+    header_home: 'Home',
+    header_releases: 'Releases',
+    header_artists: 'Artists',
+    header_news: 'News',
+    header_radio: 'Radio',
+    home_heroTitle: 'YOUR PLACE FOR THE FINEST TRIBAL,CIRCUIT & HOUSE MUSIC',
+    home_releasesButton: 'Latest Releases',
+    home_artistsButton: 'Meet the artists',
+    home_featuredReleaseHeading: 'Featured Release',
+    home_latestReleaseHeading: 'Latest',
+    home_upcomingReleasesHeading: 'Upcoming releases',
+    home_artistsReadMore: 'Read More',
+    footer_socials: 'Socials',
+    footer_contact: 'Say Hi',
+    seeAll: 'See all'
+  }
+}
