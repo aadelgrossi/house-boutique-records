662eb58efee404677b3ad112aac73412e50e6e6d
Andre Grossi <iamandregrossi@gmail.com>
2021-02-09 20:17:56 -0300

    Include public api id in client

--- src/lib/graphCmsClient.ts
+++ src/lib/graphCmsClient.ts
@@ -1,7 +1,7 @@
 import { GraphQLClient } from 'graphql-request'
 
 export default new GraphQLClient(
-  `https://api-ca-central-1.graphcms.com/v2/${process.env.NEXT_PUBLIC_GRAPH_CMS_ID}/master`,
+  `https://api-ca-central-1.graphcms.com/v2/ckkncuqsi8d6g01xo82qcgqiw/master`,
   {
     headers: {
       Authorization: `Bearer ${process.env.NEXT_PUBLIC_GRAPH_CMS_AUTH_TOKEN}`
