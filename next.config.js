b48a12c80a0e2e54507a1a39272b6f074fed321d
Andre Grossi <iamandregrossi@gmail.com>
2021-02-09 20:17:26 -0300

    Remove fixed configs in next.config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  distDir: 'build',
-  publicRuntimeConfig: {
-    // add your public runtime environment variables here with NEXT_PUBLIC_*** prefix
-  },
-  webpack: config => {
-    // extend your webpack configuration here
-    return config
-  },
   i18n: {
     defaultLocale: 'pt',
     locales: ['en', 'pt']
