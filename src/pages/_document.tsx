eef08852c1aeeaa6ec958ee716e87a870cbe2b86
Andre Grossi <iamandregrossi@gmail.com>
2021-02-10 01:50:35 -0300

    Add favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -54,6 +54,31 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Yantramanav:wght@100;300;400;700&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Anton&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#dc2430" />
+          <meta name="msapplication-TileColor" content="#dc2430" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
