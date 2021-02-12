8aa3a63be3e1ff418abdcca90b2f17962595b182
Andre Grossi <iamandregrossi@gmail.com>
2021-02-11 23:39:47 -0300

    Set min width for ReleaseCard

--- src/components/ReleaseCard/styles.ts
+++ src/components/ReleaseCard/styles.ts
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  min-width: 180px;
+  width: fit-content;
 `
 
 export const ImageWrapper = styled.div`
