87f546e3a24bef55bb5ee362e367cbe3646d52d2
Andre Grossi <iamandregrossi@gmail.com>
2022-07-29 22:46:29 -0300

    fix(debouce): Disable no-explicit-any

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,9 +1,10 @@
-export function debounce<F extends (...params: unknown[]) => void>(
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export function debounce<F extends (...params: any[]) => void>(
   fn: F,
   delay: number
 ): F {
   let timeoutID: number
-  return function (this: unknown, ...args: unknown[]) {
+  return function (this: any, ...args: any[]) {
     clearTimeout(timeoutID)
     timeoutID = window.setTimeout(() => fn.apply(this, args), delay)
   } as F
